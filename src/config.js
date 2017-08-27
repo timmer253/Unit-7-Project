a4fa3629dcf80dfc2a320b8ff2c436f41f4b355b
timmer253 <timmer253@yahoo.com>
2017-08-27 10:10:50 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,1 @@
-module.exports = {
-    "consumer_key": "uobJpOJFj3e1fq0IqSsqpwZKK",
-    "consumer_secret": "y6S0wtREpYb1Y55lMI88DxF1biwUDGaHtj8uNsGnYzhLYCyGfJ",
-    "access_token": "56751692-JoXkl9CaRM6DZOvsgLy6iG1z8MU8cFtXiONhLu2Xi",
-    "access_token_secret": "SWXt3N3IEUintrvrhl35LIqQKbZyG7I3JlWE8LzNi28BI"
-};
-/**
- * Created by Anon on 8/26/2017.
- */
+
